ce3a1e7a1446209cfc881c920596f45470466599
ZLYMichael <zlymike@outlook.com>
2017-06-15 00:07:28 +0000

    added a count for character limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(function() {
+    $("section.new-tweet").find('textarea').on("keyup keydown", function() {
+        const maxCount = 140;
+        const cRemain = maxCount - $(this).val().length;
+        const counter = $(this).parent().find(".counter");
+        counter.text(cRemain);
+        if(cRemain < 0) {
+            counter.addClass("over-count");
+        } else {
+            counter.removeClass("over-count");
+        }
+    });
+});
